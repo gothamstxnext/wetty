9993012602b95cc09f47d6575eae2bfe0f23c7a1
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-08-27 14:33:25 +0200

    allways ssh

--- app.js
+++ app.js
@@ -111,19 +111,11 @@ wss.on('request', function(request) {
     conn.on('message', function(msg) {
         var data = JSON.parse(msg.utf8Data);
         if (!term) {
-            if (process.getuid() == 0) {
-                term = pty.spawn('/bin/login', [], {
-                    name: 'xterm-256color',
-                    cols: 80,
-                    rows: 30
-                });
-            } else {
-                term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', 'PreferredAuthentications=' + sshauth], {
-                    name: 'xterm-256color',
-                    cols: 80,
-                    rows: 30
-                });
-            }
+            term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', 'PreferredAuthentications=' + sshauth], {
+                name: 'xterm-256color',
+                cols: 80,
+                rows: 30
+            });
             console.log((new Date()) + " PID=" + term.pid + " STARTED on behalf of user=" + sshuser)
             term.on('data', function(data) {
                 conn.send(JSON.stringify({
